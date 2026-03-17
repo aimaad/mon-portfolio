6bfc0a5b37d48a1646f5c427884544c17fad25f5
aimaad <106041637+aimaad@users.noreply.github.com>
2026-03-17 23:31:32 +0000

    Update PDF file path for resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Bouchnef_ImadEddine_CV_Alternance .pdf";
+import pdf from "../../Assets/../Assets/Cv_Bouchnef_imadeddine_java_angular.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
